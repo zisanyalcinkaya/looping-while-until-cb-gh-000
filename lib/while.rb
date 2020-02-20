cd0d05bbc01cb742e4fcfcc3451dc2db58c43871
Zisan Yalcinkaya <zisan.yalcinkaya@bahcesehir.edu.tr>
2020-02-20 13:59:55 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,9 +1,9 @@
 def using_while
   levitation_force = 6
-  counter = 10
-  while levitation_force<=counter do
-    levitation_force+=1
+  while levitation_force<=10 do
+    
     puts "Wingardium Leviosa"
+      levitation_force+=1
   end
   #your code here
 end
